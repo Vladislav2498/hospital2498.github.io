4c19aaef8a1b833ae93ee5006c497a97daeefa2b
Oleksandr Altynnikov <siolent.ou@gmail.com>
2022-08-12 00:28:19 +0300

    add ready step project

--- src/js/classes/modal-lera.js
+++ src/js/classes/modal-lera.js
@@ -3,6 +3,8 @@
 import checkCard from "../functions/checkAndRenderClass.js";
 import checkFields from "../functions/checkForRequiredFields.js";
 import visitsArray from "../index.js";
+import chooseDoctorListener from "../functions/chooseDoctorListener.js";
+import getInputValuesRecieveRes from "../functions/getInputValuesRecieveRes.js";
 
 class ModalLera {
   constructor() {
@@ -119,7 +121,7 @@ class ModalLera {
     document.querySelector("body").append(this.modalBackground);
   }
 }
-class visitDentist extends ModalLera {
+export class visitDentist extends ModalLera {
   constructor() {
     super();
     this.dentistContainer = document.createElement("div");
@@ -147,7 +149,7 @@ class visitDentist extends ModalLera {
     super.render();
   }
 }
-class visitCardiologist extends ModalLera {
+export class visitCardiologist extends ModalLera {
   constructor() {
     super();
     this.cardiologistContainer = document.createElement("div");
@@ -190,7 +192,7 @@ class visitCardiologist extends ModalLera {
     super.render();
   }
 }
-class visitTherapist extends ModalLera {
+export class visitTherapist extends ModalLera {
   constructor(container) {
     super();
   }
@@ -219,194 +221,5 @@ class visitTherapist extends ModalLera {
     super.render();
   }
 }
-const chooseDoctorListener = (targetValue, currentModal) => {
-  if (targetValue === "1") {
-    currentModal.deleteModal();
-    new visitDentist().render();
-  } else if (targetValue === "2") {
-    currentModal.deleteModal();
-    new visitTherapist().render();
-  } else if (targetValue === "3") {
-    currentModal.deleteModal();
-    new visitCardiologist().render();
-  } else {
-    currentModal.deleteModal();
-    new ModalLera().render();
-  }
-};
-const getInputValuesRecieveRes = () => {
-  const searchDoctor = document.querySelector("#select-doctor")?.value;
-  const searchDoctorText = document.querySelector("#select-doctor");
-  const doctorName = searchDoctorText.querySelector("[selected]").textContent;
-  const doctor = searchDoctorText.querySelector("[selected]").value;
-  const name = document.querySelector(".form-control")?.value;
-  const purpose = document.querySelector("#inputWorriesDentist")?.value;
-  const description = document.querySelector("#inputDescriptionDentist")?.value;
-  const urgency = document.querySelector("#inputUrgencyDentist")?.value;
-  const lastVisit = document.querySelector("#inputLastVisitDentist")?.value;
-  const ageTherapist = document.querySelector("#inputAgeTherapist")?.value;
-  const ageCardiologist = document.querySelector(
-    "#inputAgeCardiologist"
-  )?.value;
-  const pressure = document.querySelector("#inputPressureCardiologist")?.value;
-  const massIndex = document.querySelector("#inputIndexCardiologist")?.value;
-  const diseases = document.querySelector("#inputDiseasesCardiologist")?.value;
-  const status = "Open";
 
-  const checkInputs = checkFields(
-    doctor,
-    name,
-    purpose,
-    description,
-    lastVisit,
-    ageCardiologist,
-    ageTherapist,
-    pressure,
-    massIndex,
-    diseases
-  );
-
-  if (checkInputs) {
-    postRequest(
-      doctorName,
-      searchDoctor,
-      name,
-      purpose,
-      description,
-      urgency,
-      lastVisit,
-      ageTherapist,
-      ageCardiologist,
-      pressure,
-      massIndex,
-      diseases,
-      status
-    );
-  } else {
-    document.querySelector(".requiredFileds__error")?.remove();
-    const errContainer = document.querySelector(".modal").firstChild;
-    errContainer.insertAdjacentHTML(
-      "beforeend",
-      `
-    <div class="requiredFileds__error"> Please, complete all fields before submiting</div>
-    `
-    );
-  }
-};
-
-const postRequest = async (
-  doctorName,
-  searchDoctor,
-  name,
-  purpose,
-  description,
-  urgency,
-  lastVisit,
-  ageTherapist,
-  ageCardiologist,
-  pressure,
-  massIndex,
-  diseases,
-  status
-) => {
-  if (doctorName === "Dentist") {
-    const recieveData = await fetch(
-      "https://ajax.test-danit.com/api/v2/cards",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${localStorage.getItem("Authorization")}`,
-        },
-        body: JSON.stringify({
-          doctor: `${doctorName}`,
-          name: `${name}`,
-          purpose: `${purpose}`,
-          description: `${description}`,
-          urgency: `${urgency}`,
-          lastVisit: `${lastVisit}`,
-          status: "Open",
-        }),
-      }
-    ).then((response) => response.json());
-    console.log(recieveData);
-    checkCard("Dentist", recieveData);
-    if (visitsArray.length === 0) {
-      document.querySelector(".header__logIn--btn").innerText = "Create Visit";
-      document.querySelector(
-        ".card_container"
-      ).innerHTML = `<h2 class="empty-card">the visits is empty</h2>`;
-    } else {
-      const err = document.querySelector(".empty-card")?.remove();
-    }
-    document.querySelector(".modal-background").remove();
-  } else if (doctorName === "Therapist") {
-    console.log(ageTherapist);
-    const recieveData = await fetch(
-      "https://ajax.test-danit.com/api/v2/cards",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${localStorage.getItem("Authorization")}`,
-        },
-        body: JSON.stringify({
-          doctor: `${doctorName}`,
-          name: `${name}`,
-          purpose: `${purpose}`,
-          description: `${description}`,
-          urgency: `${urgency}`,
-          ageTherapist: `${ageTherapist}`,
-          status: "Open",
-        }),
-      }
-    ).then((response) => response.json());
-    console.log(recieveData);
-    checkCard("Therapist", recieveData);
-    if (visitsArray.length === 0) {
-      document.querySelector(".header__logIn--btn").innerText = "Create Visit";
-      document.querySelector(
-        ".card_container"
-      ).innerHTML = `<h2 class="empty-card">the visits is empty</h2>`;
-    } else {
-      const err = document.querySelector(".empty-card")?.remove();
-    }
-    document.querySelector(".modal-background").remove();
-  } else if (doctorName === "Cardiologist") {
-    const recieveData = await fetch(
-      "https://ajax.test-danit.com/api/v2/cards",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${localStorage.getItem("Authorization")}`,
-        },
-        body: JSON.stringify({
-          doctor: `${doctorName}`,
-          name: `${name}`,
-          purpose: `${purpose}`,
-          description: `${description}`,
-          urgency: `${urgency}`,
-          ageCardiologist: `${ageCardiologist}`,
-          pressure: `${pressure}`,
-          massIndex: `${massIndex}`,
-          diseases: `${diseases}`,
-          status: "Open",
-        }),
-      }
-    ).then((response) => response.json());
-    document.querySelector(".modal-background").remove();
-    checkCard("Cardiologist", recieveData);
-    if (visitsArray.length === 0) {
-      document.querySelector(".header__logIn--btn").innerText = "Create Visit";
-      document.querySelector(
-        ".card_container"
-      ).innerHTML = `<h2 class="empty-card">the visits is empty</h2>`;
-    } else {
-      const err = document.querySelector(".empty-card")?.remove();
-    }
-  } else {
-    console.log("error post information");
-  }
-};
 export default ModalLera;
